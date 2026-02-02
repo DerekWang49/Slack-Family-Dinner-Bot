e7558dc1f267d974435bfbe5197721ea7aea521f
Derek <derek.m.wang2@gmail.com>
2026-02-01 22:20:07 -0600

    Fixed scheduler

--- app.py
+++ app.py
@@ -235,8 +235,7 @@ if __name__ == "__main__":
     scheduler = BackgroundScheduler(timezone=timezone("US/Central"))
     # TODO: Add day
     scheduler.add_job(send_scheduled_message, "cron", day_of_week='mon', hour=11, minute=00)
-    scheduler.add_job(send_scheduled_message, "cron", day_of_week='fri', hour=11, minute=00)
-    scheduler.add_job(send_direct_message, "interval", seconds=30)
+    scheduler.add_job(send_direct_message, "cron", day_of_week='fri', hour=11, minute=00)
     # scheduler.add_job(send_scheduled_message, "interval", seconds=30)
     scheduler.start()
     # Get app token from environment variable
